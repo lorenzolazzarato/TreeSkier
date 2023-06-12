036a701b4cb41696cc800b074ef5833e045a7e83
Bertini00 <bertini.luca00@gmail.com>
2023-06-12 16:39:30 +0200

    Added touch position recognition (right or left of the screen)

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -9,7 +10,7 @@ public class CharacterController : MonoBehaviour
     private IdContainer _IdProvider;
 
     [SerializeField]
-    private float _MaxTimeToSwipe = 1;
+    private float _MaxTimeToSwipeInSeconds = 1;
 
     [SerializeField]
     private float _VerticalDistanceForSwipe = 50;
@@ -26,9 +27,17 @@ public class CharacterController : MonoBehaviour
     // Time when the touching starts
     private float _timeStart;
 
+    // Need the camera to calculate where the touch is
+    private ICinemachineCamera _camera;
+
     private void Awake()
     {
         _gameplayInputProvider = PlayerController.Instance.GetInput<GameplayInputProvider>(_IdProvider.Id);
+    }
+
+    private void Start()
+    {
+        _camera = CinemachineCore.Instance.GetActiveBrain(0).ActiveVirtualCamera;
     }
 
     private void OnEnable()
@@ -57,9 +66,16 @@ public class CharacterController : MonoBehaviour
 
         //Debug.Log("Move Started");
 
-        // Camera camera = Camera.main;
-        // Debug.Log(camera.orthographicSize * camera.aspect);
+        //Debug.Log(Screen.width / 2);
 
+        if (value.x < Screen.width / 2) 
+        {
+            Debug.Log("Moving Left");
+        }
+        else
+        {
+            Debug.Log("Moving Right");
+        }
 
     }
 
@@ -92,7 +108,7 @@ public class CharacterController : MonoBehaviour
 
         //Debug.LogFormat("Checking the swipe, time for the swipe {0}", time);
 
-        if (Math.Abs(start.y - finish.y) > _VerticalDistanceForSwipe && Math.Abs(start.x - finish.x) < _HorizontalDistanceForSwipe && time < _MaxTimeToSwipe)
+        if (Math.Abs(start.y - finish.y) > _VerticalDistanceForSwipe && Math.Abs(start.x - finish.x) < _HorizontalDistanceForSwipe && time < _MaxTimeToSwipeInSeconds)
         {
             //Debug.Log("Swipe Counts");
 
