3112a729ee70adb1708e7098ed4e6df5d111c664
Bertini00 <bertini.luca00@gmail.com>
2023-07-26 14:36:46 +0200

    added second feature: moving skier

---
+++ Assets/Scripts/Gameplay/EnemySkier/EnemySkierScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySkierScript : HittableObject
+{
+
+    
+    [SerializeField]
+    private float _MinXSpeed = -1f;
+
+    [SerializeField] 
+    private float _MaxXSpeed = 1f;
+
+
+
+    [SerializeField]
+    private float _MovementSpeed = 3f;
+
+    
+    private float xSpeed = 0;
+
+    private SpriteRenderer _sprite;
+    
+
+    public override void Setup()
+    {
+        base.Setup();
+
+        xSpeed = Random.Range(_MinXSpeed, _MaxXSpeed);
+
+        _sprite = GetComponentInChildren<SpriteRenderer>();
+
+        _sprite.flipX = xSpeed > 0;
+    }
+
+    public override void Clear()
+    {
+        base.Clear();
+
+        xSpeed = 0;
+    }
+
+    void Update()
+    {
+        base.Update();
+        transform.Translate(Vector3.right * xSpeed * Time.deltaTime * _MovementSpeed);
+    }
+}
